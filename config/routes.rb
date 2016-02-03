09ba9ca556f67b312454e128ac6a6e2e746355bd
dgiraldo313 <dgiraldo313@drew.edu>
2016-02-02 21:59:58 -0500

    Basic admin panel working
    Folders are very messy at the moment. Lots of stuff commented out,
    especially where the ember framework was located. Might still consider
    leaving ember in case that I can use the in the game pages. Also need
    to do a good amount of work on the routes and adding attributes for the
    users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 EmberHelloWorld::Application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users
-      resources :players
-      resources :admins
-    end
-  end
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users
+  #     resources :players
+  #     resources :admins
+  #   end
+  # end
+  # resources :users
   root to: 'home#index'
-  get '*path', to: 'home#index'
+  # get '*path', to: 'home#index'
 end
