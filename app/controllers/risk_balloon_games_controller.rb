8b8df6aaf19b4ce4ab3da5159827a86e899ae7b6
dgiraldo313 <dgiraldo313@drew.edu>
2016-02-14 20:11:49 -0500

    added page authentication for research page and risk balloon page

--- app/controllers/risk_balloon_games_controller.rb
+++ app/controllers/risk_balloon_games_controller.rb
@@ -1,6 +1,8 @@
 class RiskBalloonGamesController < ApplicationController
-
+  before_action :require_research
+  respond_to :json
   def show
     @risk_balloon_game= RiskBalloonGame.find(params[:id])
+    respond_with  RiskBalloonGame.find(params[:id])
   end
 end
