0dd16543493b14178e52030893a3385644839f11
dgiraldo313 <dgiraldo313@drew.edu>
2016-02-14 20:14:39 -0500

    json responder

--- app/controllers/risk_balloon_games_controller.rb
+++ app/controllers/risk_balloon_games_controller.rb
@@ -1,8 +1,9 @@
 class RiskBalloonGamesController < ApplicationController
   before_action :require_research
-  respond_to :json
+  respond_to :json, :html
+
   def show
     @risk_balloon_game= RiskBalloonGame.find(params[:id])
-    respond_with  RiskBalloonGame.find(params[:id])
+    respond_with  @risk_balloon_game
   end
 end
