8e5526d38d48549c7a6fc302f539f07a230ed96c
dgiraldo313 <dgiraldo313@drew.edu>
2016-02-14 19:38:51 -0500

    add research_id field to players table

--- db/migrate/20160210211310_fix_players_table.rb
+++ db/migrate/20160210211310_fix_players_table.rb
@@ -1,6 +1,6 @@
 class FixPlayersTable < ActiveRecord::Migration
   def change
-    # add_column :players, :research_id, :integer
+    add_column :players, :research_id, :integer
     remove_column :players, :PlayerID
   end
 end
